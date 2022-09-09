4570c517be16e2669deb827596896a0a21ce7d7b
unknown <1624055384@qq.com>
2022-09-10 00:35:51 +0800

    '数据库配置'

--- myblog/myblog/settings.py
+++ myblog/myblog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'jed27x(mrbg+b!dvupq-d)(a)mqd_^a4wqmw15z3&&s&)01y9v'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myblog',
 ]
 
 MIDDLEWARE = [
@@ -69,16 +70,20 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'myblog.wsgi.application'
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'blogmgr',
+        'PASSWORD': '123456',
+        'NAME': 'myblog'
+    },
+}
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
 
 
 # Password validation
